69c122cb1fc30374b5c9f92f74e204e4d2d9003e
rli8145 <rli8145@gmail.com>
2026-05-19 01:18:48 -0700

    lc124: binary tree maximum path sum

---
+++ leetcode/lc124.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    int dfs(TreeNode* node, int& res) {
+        if (!node) return 0;
+        int left = dfs(node->left, res), right = dfs(node->right, res);
+        res = max(res, node->val + max(0, left) + max(0, right));
+        return node->val + max({left, right, 0});
+    }
+    int maxPathSum(TreeNode* root) {
+        int res = INT_MIN;
+        dfs(root, res);
+        return res;
+    }
+};
